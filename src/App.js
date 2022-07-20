2f01d560fcef05095e4ec08f94dba63110f5ed53
Kateryna <tsymbal.katena@mail.ru>
2022-07-19 23:43:31 -0500

    working on routong

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+import Home from "./components/Home/Home"
+import Footer from "./components/Footer/Footer"
+import Header from './components/Header/Header'
+import MovieDetail from "./components/MovieDetail/MovieDetail"
+import PageNotFound from "./components/PageNotFound/PageNotFound"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <Route path="/" component={Home}></Route>
+        <Route path="/movie/:imdbID" component={MovieDetail}></Route>
+        <Route component={PageNotFound} ></Route>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
